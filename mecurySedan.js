72ae3b8027e7ec04a28263ac320c06ff5a8d8c8a
Erick Falconi <e.alexander.f013@gmail.com>
2021-11-06 01:21:44 -0400

    finished assignment no bonus

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,46 @@
+// import '/Users/erickfalconi/Documents/js/5.5.5-activity-array-automotive/vehicleBaseClass.js'
+import Vehicle from './vehicleBaseClass.js';
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
+
+    loadPassanger(num) {
+        if (num + this.passenger < this.maximumPassengers) {
+            return this.availableRoom == true;
+        } else {
+            return this.availableRoom == false;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            console.log("Engine started...")
+            return this.started == true;
+        } else {
+            console.log("No fuel...")
+            return this.started == false;
+        }
+    }
+
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {
+            this.scheduleService == true
+            return this.scheduleService;
+        }
+    }
+}
